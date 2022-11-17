8a3487f92e8beefb7856a75e134d8a38b3f15a99
Waqas Khurshid <waqaskhurshidoffical@gmail.com>
2022-11-17 13:47:13 +0500

    change status bar color

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarColor: Colors.white,
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Expanded(flex: 40, child: Image.asset('assets/images/graph.png')),
+          Expanded(flex: 40, child: Image.asset('assets/images/graph.png')),
+        ],
+      ),
+    );
   }
 }
