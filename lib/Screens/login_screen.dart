9e9d373631ce20445e8e1622285435c6f89fdb6f
pak comp <git>
2022-11-17 23:54:57 +0500

    make different curve with different size

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -7,12 +5,13 @@ import 'package:login_signup_with_validations/Clipers/CurveClipper.dart';
 import 'package:sizer/sizer.dart';
 
 class LoginScreen extends StatelessWidget {
-   LoginScreen({Key? key}) : super(key: key);
+  LoginScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    final  mediaQuery = MediaQuery.of(context).size;
-     var  height = mediaQuery.height;
-     var  width = mediaQuery.width;
+    final mediaQuery = MediaQuery.of(context).size;
+    var height = mediaQuery.height;
+    var width = mediaQuery.width;
     return Scaffold(
       appBar: AppBar(
         systemOverlayStyle: const SystemUiOverlayStyle(
@@ -24,34 +23,23 @@ class LoginScreen extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            AnimatedCrossFade( crossFadeState: CrossFadeState.showFirst
-                 ,firstChild: Image.asset('assets/images/graph.png', height: height * 0.4,),
-                 secondChild:  const  Text(''),
-                 duration: const Duration(milliseconds: 600),
+            AnimatedCrossFade(
+              crossFadeState: CrossFadeState.showFirst,
+              firstChild: Image.asset(
+                'assets/images/graph.png',
+                height: height * 0.4,
+              ),
+              secondChild: const Text(''),
+              duration: const Duration(milliseconds: 600),
             ),
-           ClipPath(
-             clipper: UserCurveClipper(),
-             child: AnimatedCrossFade(
-               firstChild: Stack(
-                 children: [
-                   Container(color: Colors.grey,
-                     height: 270, width: 300,),
-                   Container(color: Colors.greenAccent,
-                     height: 150, width: 300,),
-                   Container(color: Colors.yellow,
-                     height: 100, width: 300,),
-                 ],
-               ),
-               secondChild: Stack(
-                children: [
-                  Container(color: Colors.black,
-                    height: 270, width: 300,),
-                ],
-               ),
-               crossFadeState: CrossFadeState.showFirst,
-               duration: const Duration(milliseconds: 300),
-             ),
-           ),
+            ClipPath(
+              clipper: UserCurveClipper(),
+              child: Container(
+                color: Colors.grey,
+                height: 300,
+                width: 300,
+              ),
+            ),
           ],
         ),
       ),
